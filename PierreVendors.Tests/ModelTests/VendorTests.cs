17b07129281a3ee23509ce64c7496f2e1f930986
marnionrails <37088724+marnionrails@users.noreply.github.com>
2021-09-25 21:38:58 -0400

    redo pass test 1, just confirm that Vendor class exists

--- PierreVendors.Tests/ModelTests/VendorTests.cs
+++ PierreVendors.Tests/ModelTests/VendorTests.cs
@@ -6,14 +6,29 @@ using System;
 namespace PierreVendors.Tests
 {
   [TestClass]
-  public class VendorTests
+  public class VendorTests :IDisposable
   {
 
+    public void Dispose()
+    {
+
+    }
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("name", "description");
+      Vendor newVendor = new Vendor();
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+
+    /* [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Marni's Bakery";
+      Vendor newVendor = new Vendor(name);
+      string updatedName = "new Name";
+      newVendor.Name = updatedName;
+      string result = newVendor.Name;
+      Assert.AreEqual(updatedName, result);
+    } */
   }
 }
